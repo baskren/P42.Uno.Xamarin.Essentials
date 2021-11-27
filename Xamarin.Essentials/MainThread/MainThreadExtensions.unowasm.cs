6fb30d01c4d43601fd51d501bd58d04f839256c9
Ben Askren <ben@buildcalc.com>
2021-11-27 16:18:54 -0500

    FIX (MainThreadExtensions.WatchForError) : WASM throws exception

--- Xamarin.Essentials/MainThread/MainThreadExtensions.unowasm.cs
+++ Xamarin.Essentials/MainThread/MainThreadExtensions.unowasm.cs
@@ -2,32 +2,33 @@
 using System.Threading;
 using System.Threading.Tasks;
 using Windows.Foundation;
+using Windows.UI.Core;
 
 namespace Xamarin.Essentials
 {
     internal static partial class MainThreadExtensions
     {
-        internal static void WatchForError(this IAsyncAction self) =>
-            self.AsTask().WatchForError();
-
-        internal static void WatchForError<T>(this IAsyncOperation<T> self) =>
-            self.AsTask().WatchForError();
+        internal static void WatchForError(this UIAsyncOperation self)
+            =>  self.CompletionSource.Task.WatchForError();
+        
 
         internal static void WatchForError(this Task self)
         {
+            
             var context = SynchronizationContext.Current;
             if (context == null)
                 return;
 
             self.ContinueWith(
                 t =>
-            {
-                var exception = t.Exception.InnerExceptions.Count > 1 ? t.Exception : t.Exception.InnerException;
+                {
+                    var exception = t.Exception.InnerExceptions.Count > 1 ? t.Exception : t.Exception.InnerException;
 
-                context.Post(e => { throw (Exception)e; }, exception);
-            }, CancellationToken.None,
+                    context.Post(e => { throw (Exception)e; }, exception);
+                }, CancellationToken.None,
                 TaskContinuationOptions.OnlyOnFaulted,
                 TaskScheduler.Default);
+            
         }
     }
 }
