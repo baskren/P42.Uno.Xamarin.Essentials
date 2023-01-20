1f08990d86e28813492e8d335eb402a11df3a0b9
Ben Askren <ben@buildcalc.com>
2023-01-20 14:08:07 -0500

    FIX : Samples.Uno.WinUi3/Samples.Windows app throws XamlResource exception on start.

--- Samples.Uno.WinUi3/Samples.Shared/View/AppInfoPage.xaml.cs
+++ Samples.Uno.WinUi3/Samples.Shared/View/AppInfoPage.xaml.cs
@@ -8,16 +8,17 @@ namespace Samples.View
         {
             InitializeComponent();
 
-            _stackPanel.Children.Add(new TextBlock { Text = ".Id.FamilyName = " + Windows.ApplicationModel.Package.Current.Id.FamilyName });
-            _stackPanel.Children.Add(new TextBlock { Text = ".Id.FullName = " + Windows.ApplicationModel.Package.Current.Id.FullName });
-            _stackPanel.Children.Add(new TextBlock { Text = ".Id.Name = " + Windows.ApplicationModel.Package.Current.Id.Name });
-            var version = Windows.ApplicationModel.Package.Current.Id.Version;
+            var id = Windows.ApplicationModel.Package.Current.Id;
+            _stackPanel.Children.Add(new TextBlock { Text = ".Id.FamilyName = " + id.FamilyName });
+            _stackPanel.Children.Add(new TextBlock { Text = ".Id.FullName = " + id.FullName });
+            _stackPanel.Children.Add(new TextBlock { Text = ".Id.Name = " + id.Name });
+            var version = id.Version;
             _stackPanel.Children.Add(new TextBlock { Text = ".Id.Version = " + $"{version.Major}.{version.Minor}.{version.Build}.{version.Revision}" });
-            _stackPanel.Children.Add(new TextBlock { Text = ".Id.Publisher = " + Windows.ApplicationModel.Package.Current.Id.Publisher });
-            _stackPanel.Children.Add(new TextBlock { Text = ".Id.PublisherId = " + Windows.ApplicationModel.Package.Current.Id.PublisherId });
-            _stackPanel.Children.Add(new TextBlock { Text = ".Id.ResourceId = " + Windows.ApplicationModel.Package.Current.Id.ResourceId });
-            _stackPanel.Children.Add(new TextBlock { Text = ".Id.Author = " + Windows.ApplicationModel.Package.Current.Id.Author });
-            _stackPanel.Children.Add(new TextBlock { Text = ".Id.ProductId = " + Windows.ApplicationModel.Package.Current.Id.ProductId });
+            _stackPanel.Children.Add(new TextBlock { Text = ".Id.Publisher = " + id.Publisher });
+            _stackPanel.Children.Add(new TextBlock { Text = ".Id.PublisherId = " + id.PublisherId });
+            _stackPanel.Children.Add(new TextBlock { Text = ".Id.ResourceId = " + id.ResourceId });
+            // _stackPanel.Children.Add(new TextBlock { Text = ".Id.Author = " + id.Author });
+            // _stackPanel.Children.Add(new TextBlock { Text = ".Id.ProductId = " + id.ProductId });
         }
     }
 }
