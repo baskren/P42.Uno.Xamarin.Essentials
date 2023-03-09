17147d18d77934928a5769fae87c4064444b157b
baskren <ben@buildcalc.com>
2023-03-09 09:13:52 -0500

    INTERMEDIATE

---
+++ Xamarin.Essentials/Types/PlatformExtensions/RectangleExtensions.wasm.win.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Drawing;
+using Windows.Foundation;
+
+namespace Xamarin.Essentials
+{
+    public static partial class RectangleExtensions
+    {
+
+        public static Rect ToPlatformRectangle(this Rectangle rect) =>
+            new Rect(rect.X, rect.Y, rect.Width, rect.Height);
+
+        public static Rect ToPlatformRectangle(this RectangleF rect) =>
+            new Rect(rect.X, rect.Y, rect.Width, rect.Height);
+    }
+}
+
---
+++ Xamarin.Essentials/Types/PlatformExtensions/SizeExtensions.wasm.win.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Drawing;
+using WindowsSize = Windows.Foundation.Size;
+
+namespace Xamarin.Essentials
+{
+    public static partial class SizeExtensions
+    {
+        public static WindowsSize ToPlatformSize(this Size size) =>
+            new WindowsSize(size.Width, size.Height);
+
+        public static WindowsSize ToPlatformSize(this SizeF size) =>
+            new WindowsSize(size.Width, size.Height);
+    }
+}
